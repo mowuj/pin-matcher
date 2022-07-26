b3d973f0513312652d9fde794ce932cf2e4460b1
mowuj <mowuj92@gmail.com>
2022-07-26 23:05:52 +0600

    pin matcher

---
+++ js/app.js
@@ -0,0 +1,52 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        // console.log('got 3 digit and calling again', pin);
+        return getPin(); 
+    }
+}
+function generatePin() {
+    const pin = getPin()
+    document.getElementById('display-pin').value = pin;
+}
+document.getElementById('key-pad').addEventListener(
+    'click', function (event) {
+        const number = event.target.innerText;
+        const calcInput = document.getElementById('typed-numbers');
+        if (isNaN(number)) {
+            if (number == 'C') {
+                calcInput.value = '';
+            }
+        }
+        else {
+            
+            const previousNumber = calcInput.value;
+            const newNumber = previousNumber + number;
+            calcInput.value = newNumber;
+        }
+        
+});
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successNotify = document.getElementById('notify-seccess');
+    const failError = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+        
+        successNotify.style.display = 'block';
+        failError.style.display='none'
+    }
+    else {
+        
+        failError.style.display = 'block';
+        successNotify.style.display = 'none';
+    }
+};
+	const incrementButton = document.getElementById("increment-btn")
+    incrementButton.addEventListener("click", function () { 
+    incrementButton.innerText = parseInt(incrementButton.innerText)+3; 
+    })
